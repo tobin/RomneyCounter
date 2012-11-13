f52234557b1ba2e40a430bb9e6d628562075ac75
katedooley <kate.l.dooley@gmail.com>
2012-11-13 09:58:23 +0100

    more data collected

--- romney_counter.sh
+++ romney_counter.sh
@@ -2,5 +2,5 @@
 
 while true; do
   ((date +"%s" | tr "\n" " ") ; (wget --quiet -O - graph.facebook.com/mittromney | grep -E -o "\"likes\":[0-9]*" | grep -E -o "[0-9]+")) | tee -a romneycount.txt
-  sleep 300
+  sleep 600
 done
